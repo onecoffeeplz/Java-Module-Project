f73074122f8c3cf46b2a86dd9196cdfa8f661700
onecoffeeplz <110420215+onecoffeeplz@users.noreply.github.com>
2024-06-25 16:39:48 +0300

    Revert "Проектная работа №1 - «24 часа Ле-Мана»"
    This reverts commit e832f54bb208ecaecc30e5e9a5b65d4249a5e9d9.

--- src/main/java/Car.java
+++
@@ -1,9 +0,0 @@
-public class Car {
-    String name;
-    int velocity;
-
-    public Car(String name, int velocity) {
-        this.name = name;
-        this.velocity = velocity;
-    }
-}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Race race = new Race();
-
-        for (int i = 1; i <= 3; i++) {
-            System.out.println("Введите название машины №" + i + ":");
-            String name = scanner.next();
-            int velocity;
-            while (true) {
-                System.out.println("Введите скорость машины №" + i + ":");
-                velocity = scanner.nextInt();
-                if (0 < velocity & velocity <= 250) {
-                    break;
-                } else {
-                    System.out.println("Неправильная скорость");
-                }
-            }
-            Car car = new Car(name, velocity);
-            race.isLeader(car);
-        }
-        System.out.println("Самая быстрая машина: " + race.getLeaderName());
-        scanner.close();
+        // ваш код начнется здесь
+        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
+        System.out.println("Привет Мир");
     }
 }
